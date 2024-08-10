a14f134feaf24ff5e4e87de87d84460698c163ec
Nasiba Zulunova <nasiba.zulunova@gmail.com>
2024-08-10 13:00:49 +0500

    add localStorage & submit on Enter

--- to-do-app/main.js
+++ to-do-app/main.js
@@ -1,15 +1,31 @@
 const input = document.querySelector("input");
-const addButton = document.querySelector("button");
+const form = document.querySelector("form");
 const list = document.querySelector("section");
 const placeholder = document.querySelector("p");
+// localStorage.clear();
+if (localStorage.length == 0) {
+  localStorage.setItem("index", 0);
+}
+else {
+  const keys = [];
+  for (let i = 1; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (!Number.isNaN(key * 1)) {
+      keys.push(Number.parseInt(key));
+    }
+  }
+  for (let i of keys.sort()) {
+    addListItem(localStorage.getItem(i), i);
+  }
+}
 
-addButton.onclick = () => {
-  input.focus();
-  if (input.value == '')
-    return;
-  placeholder.style.visibility = 'hidden';
-  const task = input.value;
-  input.value = "";
+if (list.children.length == 0) {
+  placeholder.textContent = "Nothing to do.";
+}
+
+function addListItem (text, index) {
+  placeholder.textContent = '';
+
   const listItem = document.createElement("div");
   const checkboxWrapper = document.createElement("div");
   const checkbox = document.createElement("input");
@@ -22,22 +38,57 @@ addButton.onclick = () => {
   listItem.appendChild(deleteButton);
   list.appendChild(listItem);
 
-  listItem.classList.add("input-group", "mb-3")
-  checkboxWrapper.classList.add("input-group-text")
-  itemValue.value = task;
-  itemValue.classList.add("form-control")
+  listItem.classList.add("input-group", "mb-3");
+  checkboxWrapper.classList.add("input-group-text");
+  checkbox.classList.add("form-check-input", "mt-0");
+  itemValue.classList.add("form-control");
+  deleteButton.classList.add("btn", "btn-outline-danger");
+
   checkbox.type = "checkbox";
-  checkbox.classList.add("form-check-input", "mt-0")
-  checkbox.onchange = () => {
-    console.log("checkbox changed");
-    itemValue.style.textDecoration = checkbox.checked ? "line-through" : "none";
-  }
+  checkbox.checked = JSON.parse(localStorage.getItem(index + " checked"));
+
+  itemValue.value = text;
+  itemValue.style.textDecoration = checkbox.checked ? "line-through" : "none";
+
   deleteButton.textContent = "Delete";
-  deleteButton.classList.add("btn", "btn-outline-danger")
+
+  checkbox.onchange = () => {
+    itemValue.style.textDecoration = checkbox.checked ? "line-through" : "none";
+    localStorage.setItem(index + " checked", checkbox.checked);
+  }
+
+  itemValue.onblur = () => {
+    localStorage.setItem(index, itemValue.value);
+  }
+
+  itemValue.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      localStorage.setItem(index, itemValue.value);
+      itemValue.blur();
+    }
+  });
+  
   deleteButton.onclick = () => {
     list.removeChild(listItem);
+    localStorage.removeItem(index);
+    localStorage.removeItem(index + "checked");
     if (list.children.length == 0) {
-      placeholder.style.visibility = 'visible';
+      placeholder.textContent = 'Nothing to do.';
     }
   }
 }
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  input.focus();
+  if (input.value == '')
+    return;
+  const task = input.value;
+  input.value = "";
+
+  const index = Number.parseInt(localStorage.getItem("index"));
+  localStorage.setItem(index, task);
+  localStorage.setItem(index + " checked", "false");
+  localStorage.setItem("index", index + 1);
+  addListItem(task, index);
+});
