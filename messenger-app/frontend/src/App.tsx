b899366a666358fe3006a9467a969b2ec3562b87
Nasiba Zulunova <nasiba.zulunova@gmail.com>
2025-01-27 14:23:21 +0500

    revert test commits

--- messenger-app/frontend/src/App.tsx
+++ messenger-app/frontend/src/App.tsx
@@ -8,7 +8,7 @@ import Signup from "./pages/Signup.tsx";
 function App() {
   const { loggedIn, loading } = useAuth();
   if (loading) {
-    return <div>Loading... test</div>;
+    return <div>Loading...</div>;
   }
   return (
     <BrowserRouter>
